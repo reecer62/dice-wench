1c307e8affa15328d92ef7bb530fb2d30eb3483f
Reece Emero <reece.emero98@gmail.com>
2019-08-21 18:40:44 -0400

    More setup, add auth file template and linted js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const Discord = require("discord.js")
-const auth = require("./auth.json")
+const Discord = require('discord.js')
+const auth = require('./auth.json')
 
-var bot = new Discord.Client()
+const bot = new Discord.Client()
 
-bot.on("message", msg => {
-    console.log(msg.channel.name)
-    console.log(msg.content)
+bot.on('message', msg => {
+	console.log(msg.channel.name)
+	console.log(msg.content)
 })
 
-bot.on("ready",() => {
-    console.log("Connected")
+bot.on('ready', () => {
+	console.log('Connected')
 })
+
 bot.login(auth.token)
-
-console.log('init')
