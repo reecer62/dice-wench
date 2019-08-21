febdf7896966b11745aea0f4cc4421731a21ff80
Joshua-Gordon <joshua.sam.gordon@gmail.com>
2019-08-21 18:32:41 -0400

    Create bot client

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const Discord = require("discord.js")
+const auth = require("./auth.json")
+
+var bot = new Discord.Client()
+
+bot.on("message", msg => {
+    console.log(msg.channel.name)
+    console.log(msg.content)
+})
+
+bot.on("ready",() => {
+    console.log("Connected")
+})
+bot.login(auth.token)
+
 console.log('init')
