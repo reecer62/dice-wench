94d2bb95bc9def17ca2dbe0f1f44b5f69bfbca20
Joshua-Gordon <joshua.sam.gordon@gmail.com>
2019-08-21 18:49:46 -0400

    Add echo for testing

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const auth = require('./auth.json')
 const bot = new Discord.Client()
 
 bot.on('message', msg => {
+    if(msg.author.id == bot.user.id) {
+        return
+    }
 	console.log(msg.channel.name)
 	console.log(msg.content)
+    msg.channel.send(msg.content)
 })
 
 bot.on('ready', () => {
