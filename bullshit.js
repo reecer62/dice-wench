d4bef5fb44b5cad59880f38465b12cd651c43adc
Reece Emero <reece.emero98@gmail.com>
2019-08-22 14:42:20 -0400

    Added docs

--- bullshit.js
+++ bullshit.js
@@ -1,5 +1,10 @@
 const { execSync } = require('child_process')
 
+/**
+ * Gets a random line from a file (eg. quotes, bullshit table, madness table)
+ *
+ * @param {String} fileName - name of file to get a random line from
+ */
 function bullshit(fileName) {
 	const bs = execSync(`py bullshit.py ${fileName}`)
 	console.log('BS: ' + bs)
