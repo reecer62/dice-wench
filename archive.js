5c5be064e6ad87e5f11297db6ba999f74be24806
Reece Emero <reece.emero98@gmail.com>
2019-08-25 22:54:34 -0400

    Lint

--- archive.js
+++ archive.js
@@ -1,71 +1,71 @@
 const request = require('request')
 
-const root = "http://dnd.cosi.clarkson.edu/api"
+const root = 'http://dnd.cosi.clarkson.edu/api'
 
 function randomItem() {
-    return new Promise((resolve,reject) => {
+	return new Promise((resolve, reject) => {
 
-        request(root + "/Items/random",function(err,resp,body) {
+		request(root + '/Items/random', function(err, resp, body) {
 
-            if(err) {
-                reject(err)
-            }
+			if (err) {
+				reject(err)
+			}
 
-            //console.log(resp)
+			// console.log(resp)
 
-            resolve(JSON.parse(body))
+			resolve(JSON.parse(body))
 
-        })
+		})
 
-    })
+	})
 }
 
-function specificItem(name){
-    return new Promise((resolve,reject) => {
+function specificItem(name) {
+	return new Promise((resolve, reject) => {
 
-        request(root + "/Items/" + name,function(err,resp,body) {
+		request(root + '/Items/' + name, function(err, resp, body) {
 
-            if(err) {
-                reject(err)
-            }
+			if (err) {
+				reject(err)
+			}
 
-            //console.log(resp)
+			// console.log(resp)
 
-            resolve(JSON.parse(body))
+			resolve(JSON.parse(body))
 
-        })
+		})
 
-    })
+	})
 }
 
 function formatItem(item) {
-    let name = item["title"]
-    console.log(item)
-    console.log(name)
-    let sections = item["body"]
-    let text = []
-    for(var sec in sections) {
-        console.log("sec: " + sec)
-        text.push("----------------")
-        if(sec != "null") {
-            text.push("*"+sec+"*")
-        }
-        text.push(sections[sec])
-        if(sections[sec].length > 2000) {
-            return {
-                "name" : name,
-                "text" : ["Item is too large","Link: http://" + item["link"]]
-            }
-        }
-        console.log("sections: " + JSON.stringify(sections))
-        console.log("sections.sec: " + sections[sec])
-    }
-    text.push("----------------")
-    text.push("Link: http://" + item["link"])
-    return {
-        "name" : name,
-        "text" : text
-    }
+	const name = item['title']
+	console.log(item)
+	console.log(name)
+	const sections = item['body']
+	const text = []
+	for (const sec in sections) {
+		console.log('sec: ' + sec)
+		text.push('----------------')
+		if (sec !== 'null') {
+			text.push('*' + sec + '*')
+		}
+		text.push(sections[sec])
+		if (sections[sec].length > 2000) {
+			return {
+				'name' : name,
+				'text' : ['Item is too large', 'Link: http://' + item['link']],
+			}
+		}
+		console.log('sections: ' + JSON.stringify(sections))
+		console.log('sections.sec: ' + sections[sec])
+	}
+	text.push('----------------')
+	text.push('Link: http://' + item['link'])
+	return {
+		'name' : name,
+		'text' : text,
+	}
 }
 
 
