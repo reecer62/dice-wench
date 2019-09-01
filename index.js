d992836ab4e4499cb8bd712dd54a649f1982a7c1
Joshua-Gordon <joshua.sam.gordon@gmail.com>
2019-09-01 17:33:37 -0400

    Add name

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ bot.on('message', msg => {
 		text = text.slice(1)
 	} else if (text.toLowerCase().startsWith('wench, ')) {
 		text = text.slice(7)
+	} else if (text.toLowerCase().startsWith('bitch, ')) {
+        text = text.slice(7)
 	} else {
 		return
 	}
